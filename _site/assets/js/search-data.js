8f8d399342ed4c7e7444b7fb8e5fa5921c678e19
raghavan <itsrag@gmail.com>
2021-01-28 11:03:37 -0800

    Initial commit

---
+++ _site/assets/js/search-data.js
@@ -0,0 +1,6 @@
+var store = [{
+        "title": "Bible Journaling",
+        "excerpt":"Perhaps you feel a connection to God through your readings of the Bible or in the prayer that follows. Another beautiful way to connect and explore the questions and understandings that arise in your journey of faith is in journaling. There are times when your Bible study may be good...","categories": ["Bible"],
+        "tags": [],
+        "url": "/bible/BibleJournaling/"
+      }]
